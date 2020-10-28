1a0ab93320f18624175fb0a214c6a26697e52d60
swgarz <delagarzadgc@gmail.com>
2020-10-28 10:06:22 -0600

    header first part

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>This is my app</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+       <div></div>
+       <div>
+            <ul>
+              <li>home</li>
+              <li>contact</li>
+              <li>email</li>
+            </ul>
+       </div>
+
+    </header>
   );
 }
 
